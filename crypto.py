4b3b97823a837abc2b5cdf613de4da80f82a7f2e
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2021-06-22 22:12:12 -0400

    Real time update

--- crypto.py
+++ crypto.py
@@ -1,14 +1,28 @@
-import json
+import time
 import requests
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
 
 @app.route("/")
 def hello_crypto():
-    response = requests.get("https://api.coinpaprika.com/v1/tickers/dai-dai")
-    crytpo_dai = json.loads(response.text)
-    return render_template("index.html", crypto=crytpo_dai)
+    """Create static html."""
+    url = "https://api.coinpaprika.com/v1/tickers/dai-dai"
+    crypto_dai = requests.get(url).json()
+    return render_template("index.html", crypto=crypto_dai)
+
+@socketio.on('start')
+def handle_start():
+    """Update html every 1 second."""
+    while True:
+        url = "https://api.coinpaprika.com/v1/tickers/dai-dai"
+        crypto_dai = requests.get(url).json()
+        print("Updating...")
+        emit('update', crypto_dai)
+        time.sleep(1)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app)
